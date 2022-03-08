17c47b10b9cdac2aa738f850abce3fbdeb922e08
brada1604 <bagusnugroho104@gmail.com>
2022-03-08 10:42:23 +0700

    add cooming soon

---
+++ index.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Samudra Catering Official</title>
+
+    <link rel="manifest" href="images/favicons/manifest.json">
+    <link rel="shortcut icon" href="images/favicon.ico">
+    <meta name="msapplication-TileColor" content="#2d89ef">
+    <meta name="msapplication-config" content="images/favicons/browserconfig.xml">
+    <meta name="theme-color" content="#ffffff">
+
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- fontawesome -->
+    <script src="https://use.fontawesome.com/3db7fc1628.js"></script>
+
+    
+    <!-- google font -->
+    <link href='http://fonts.googleapis.com/css?family=Cabin:400,700' rel='stylesheet' type='text/css'>
+
+    <!-- custom css -->
+    <link href="css/custom.css" rel="stylesheet"/>
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+    <div class="se-pre-con"></div>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <div class="header-logo-wrapper">
+            <img src="images/Multikreatif Logo Transparent.png" alt="mki" title="Multi Kreatif Indonesia" class="img-responsive center-block" width="50%" />
+          </div>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col-md-12">
+        <h1 class="text-center">Kami akan kembali dengan fitur baru dan menarik!</h1>
+        <h3 class="text-center">Berusaha menjadi yang terunik</h3>
+        </div>
+      </div>
+  
+      <div class="row">
+        <div class="col-md-12">
+          <div id="counter_wrapper">
+            <div class="text-center" id="counter"></div>
+          </div>
+        </div>
+      </div>
+
+    <div class="row">
+      <div class="col-md-12">
+        <div class="social-media-wrapper text-center">
+          <a href="#"><i class="fa fa-pinterest"></i></a>
+          <a href="#"><i class="fa fa-facebook-official"></i></a>
+          <a href="#"><i class="fa fa-google-plus-official"></i></a>
+          <a href="https:/instagram.com/multikreatifindonesia"><i class="fa fa-twitter"></i></a>
+		  <a href="#"><i class="fa fa-instagram"></i></a>
+        </div>
+      </div>
+    </div>
+
+    <div class="row">
+      <div class="col-md-12">
+        <div class="text-center copyright">Copyright &copy; 2021 by <a target="_blank" style="color:#fff;" href="https:/linktr.ee/multikreatifindonesia">MULTI KREATIF INDOESIA</a></div> 
+      </div>
+    </div>
+    
+    </div>
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+
+    <!-- fit text -->
+    <script type="text/javascript" src="js/jquery.fittext.js"></script>
+
+    <!-- jquery countdown -->
+    <script type="text/javascript" src="js/jquery.plugin.js"></script> 
+    <script type="text/javascript" src="js/jquery.countdown.js"></script>
+
+    <!--placeholder -->
+    <script type="text/javascript" src="js/jquery.placeholder.js"></script>
+
+    <script type="text/javascript" src="js/scripts.js"></script>
+  </body>
+</html>
