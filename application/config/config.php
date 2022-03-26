1adaf75939078a86f140a2b8932e46e5a7bcedac
Garly Nugraha <98641173+garllockhart@users.noreply.github.com>
2022-03-26 17:58:08 +0700

    Change Config

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/samudra_catering/';
+$config['base_url'] = 'http://localhost/samudra-catering/';
 
 /*
 |--------------------------------------------------------------------------
